353f7d182e33245e0e7b3a60dec289eff2619acc
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2024-03-05 18:37:38 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,17 +12,33 @@ headerMenuClose.onclick = () => {
     document.body.classList.remove("body-block")
 };
 
-function onEntry(entry) {
-    entry.forEach(change => {
+function onEntryTransform(entryTransform) {
+    entryTransform.forEach(change => {
         if (change.isIntersecting) {
             change.target.classList.add('transform-show');
         }
     });
 }
-let options = { threshold: [0.001] };
-let observer = new IntersectionObserver(onEntry, options);
-let elements = document.querySelectorAll('.transform-anim');
-for (let elm of elements) {
-    observer.observe(elm);
+let transformOptions = { threshold: [0.001] };
+let transformObserver = new IntersectionObserver(onEntryTransform, transformOptions);
+let transformElements = document.querySelectorAll('.transform-anim');
+for (let elm of transformElements) {
+    transformObserver.observe(elm);
 }
+
+function onEntryOpacity(entryOpacity) {
+    entryOpacity.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('opacity-show');
+        }
+    });
+}
+let opacityOptions = { threshold: [0.1] };
+let opacityObserver = new IntersectionObserver(onEntryOpacity, opacityOptions);
+let opacityElements = document.querySelectorAll('.opacity-anim');
+for (let elm of opacityElements) {
+    opacityObserver.observe(elm);
+}
+
+
 
