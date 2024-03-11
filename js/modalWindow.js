f5ccb45ec5eee82fc6d25a442e9673d91f6071f9
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2024-03-11 16:03:43 +0300

    Add files via upload

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,4 +1,4 @@
-let modalWindowOpen = document.querySelector('#modalWindowOpen')
+/*let modalWindowOpen = document.querySelector('#modalWindowOpen')
 let modalWindow = document.querySelector('#modalWindow')
 let modalWindowClose = document.querySelector('#modalWindowClose')
 
@@ -11,3 +11,40 @@ modalWindowClose.onclick = () => {
     modalWindow.classList.remove("open");
     document.body.classList.remove("body-block")
 };
+
+let modalShowreelOpen = document.querySelector('#modalVideoOpen')
+let modalVideo = document.querySelector('#modalVideo')
+let modalVideoClose = document.querySelector('#modalVideoClose')
+
+modalVideoOpen.onclick = () => {
+    modalVideo.classList.toggle("open");
+    document.body.classList.toggle("body-block")
+};
+
+modalVideoClose.onclick = () => {
+    modalVideo.classList.remove("open");
+    document.body.classList.remove("body-block")
+};*/
+
+function modalWindowOpen(id) {
+    let modalWindow = document.getElementById(id);
+
+    if (modalWindow.classList.contains('open')) {
+        modalWindow.classList.remove("open");
+        document.body.classList.remove("body-block")
+
+    } else {
+        modalWindow.classList.toggle("open");
+        document.body.classList.toggle("body-block")
+    }
+}
+
+function modalWindowClose(id) {
+    let modalWindow = document.getElementById(id);
+    let modalWindowClose = document.getElementById(id + '__close')
+
+    modalWindowClose.onclick = () => {
+        modalWindow.classList.remove("open");
+        document.body.classList.remove("body-block")
+    };
+}
