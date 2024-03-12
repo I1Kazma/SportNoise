1885e405b7637b2e50a4a78e597c3c8eeb1121e1
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2024-03-12 13:20:29 +0300

    Add files via upload

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,9 +1,29 @@
-function modalWindowOpen(id) {
+/*function modalWindowForm(id) {
     let modalWindow = document.getElementById(id);
     let modalWindowClose = document.getElementById(id + '__close')
     let modalWindowVideo = document.getElementById(id + '__video')
 
+    if (modalWindow.classList.contains('open')) {
+        modalWindow.classList.remove("open");
+        document.body.classList.remove("body-block")
 
+    } else {
+        modalWindow.classList.toggle("open");
+        document.body.classList.toggle("body-block")
+
+    }
+
+    modalWindowClose.onclick = () => {
+        modalWindow.classList.remove("open");
+        document.body.classList.remove("body-block")
+
+    };
+}
+
+function modalWindowVideo(id) {
+    let modalWindow = document.getElementById(id);
+    let modalWindowClose = document.getElementById(id + '__close')
+    let modalWindowVideo = document.getElementById(id + '__video')
 
     if (modalWindow.classList.contains('open')) {
         modalWindow.classList.remove("open");
@@ -20,10 +40,37 @@ function modalWindowOpen(id) {
     modalWindowClose.onclick = () => {
         modalWindow.classList.remove("open");
         document.body.classList.remove("body-block")
-        modalWindowVideo.pause()
+        modalWindowVideo.pause();
         modalWindowVideo.currentTime = 0;
     };
+}*/
 
+function toggleModal(id, manageVideo = false) {
+    let modalWindow = document.getElementById(id);
+    let modalWindowClose = document.getElementById(id + '__close');
+    let modalWindowVideo = document.getElementById(id + '__video');
 
+    if (modalWindow.classList.contains('open')) {
+        modalWindow.classList.remove("open");
+        document.body.classList.remove("body-block");
+        if (manageVideo) {
+            modalWindowVideo.pause();
+            modalWindowVideo.currentTime = 0;
+        }
+    } else {
+        modalWindow.classList.add("open");
+        document.body.classList.add("body-block");
+        if (manageVideo) {
+            modalWindowVideo.play();
+        }
+    }
 
+    modalWindowClose.onclick = () => {
+        modalWindow.classList.remove("open");
+        document.body.classList.remove("body-block");
+        if (manageVideo) {
+            modalWindowVideo.pause();
+            modalWindowVideo.currentTime = 0;
+        }
+    };
 }
