1325b02be4477ac5be3035273e99ec2015f64f2e
Ivan Kazmenko <117230564+I1Kazma@users.noreply.github.com>
2024-03-12 12:55:40 +0300

    Add files via upload

--- js/modalWindow.js
+++ js/modalWindow.js
@@ -1,50 +1,29 @@
-/*let modalWindowOpen = document.querySelector('#modalWindowOpen')
-let modalWindow = document.querySelector('#modalWindow')
-let modalWindowClose = document.querySelector('#modalWindowClose')
-
-modalWindowOpen.onclick = () => {
-    modalWindow.classList.toggle("open");
-    document.body.classList.toggle("body-block")
-};
-
-modalWindowClose.onclick = () => {
-    modalWindow.classList.remove("open");
-    document.body.classList.remove("body-block")
-};
-
-let modalShowreelOpen = document.querySelector('#modalVideoOpen')
-let modalVideo = document.querySelector('#modalVideo')
-let modalVideoClose = document.querySelector('#modalVideoClose')
-
-modalVideoOpen.onclick = () => {
-    modalVideo.classList.toggle("open");
-    document.body.classList.toggle("body-block")
-};
-
-modalVideoClose.onclick = () => {
-    modalVideo.classList.remove("open");
-    document.body.classList.remove("body-block")
-};*/
-
 function modalWindowOpen(id) {
     let modalWindow = document.getElementById(id);
+    let modalWindowClose = document.getElementById(id + '__close')
+    let modalWindowVideo = document.getElementById(id + '__video')
+
+
 
     if (modalWindow.classList.contains('open')) {
         modalWindow.classList.remove("open");
         document.body.classList.remove("body-block")
+        modalWindowVideo.pause();
+        modalWindowVideo.currentTime = 0;
 
     } else {
         modalWindow.classList.toggle("open");
         document.body.classList.toggle("body-block")
+        modalWindowVideo.play()
     }
-}
-
-function modalWindowClose(id) {
-    let modalWindow = document.getElementById(id);
-    let modalWindowClose = document.getElementById(id + '__close')
 
     modalWindowClose.onclick = () => {
         modalWindow.classList.remove("open");
         document.body.classList.remove("body-block")
+        modalWindowVideo.pause()
+        modalWindowVideo.currentTime = 0;
     };
+
+
+
 }
